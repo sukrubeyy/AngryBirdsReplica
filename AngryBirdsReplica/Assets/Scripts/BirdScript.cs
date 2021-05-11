0506f541c21a815e76b62317a7d73c94f6d2d938
sukrubeyy <sukru.beyy@outlook.com>
2021-05-12 01:33:37 +0300

    finished this project

---
+++ AngryBirdsReplica/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdScript : MonoBehaviour
+{
+    bool isPress;
+    public Rigidbody2D rb;
+    public Rigidbody2D hookRb;
+    public float maxDistance = 2f;
+    public float leftTime = .15f;
+    public GameObject bird;
+    private void Update()
+    {
+        if(isPress)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hookRb.position)>maxDistance)
+            {
+                rb.position = hookRb.position + (mousePos - hookRb.position).normalized*maxDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+    void OnMouseDown()
+    {
+        isPress = true;
+        rb.isKinematic = true;
+    }
+
+     void OnMouseUp()
+    {
+        isPress = false;
+        rb.isKinematic = false;
+        StartCoroutine(leftBird(leftTime));
+    }
+
+    IEnumerator leftBird(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(2f);
+        if(bird!=null)
+        {
+            bird.SetActive(true);
+        }
+        
+        else if(bird==null || enemyScript.enemiesCount<=0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ AngryBirdsReplica/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemyScript : MonoBehaviour
+{
+    const float healt = 4f;
+    public GameObject deathEffect;
+    public static int enemiesCount;
+
+    private void Start()
+    {
+        enemiesCount++;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude>healt)
+        {
+            Die();
+        }
+      
+    }
+    private void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        enemiesCount--;
+         if(enemiesCount<=0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        Destroy(this.gameObject);
+    }
+}
